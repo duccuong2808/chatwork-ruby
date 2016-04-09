e1b49371dd26b996f7b6d3c9380a59636e9c12bb
duc cuong <duccuong2808@gmail.com>
2016-04-09 19:21:45 +0700

    add method delete room

--- lib/chatwork/room.rb
+++ lib/chatwork/room.rb
@@ -9,5 +9,20 @@ module ChatWork
     def path
       "/rooms"
     end
+
+    def self.delete(params = {})
+      @assign_path = parse_if_hash_key_exists(path << "/" << params[:room_id].to_s , params, :room_id)
+      attach_nested_resource_id(params)
+      action = convert ChatWork.client.delete(@assign_path, params)
+      if action == APIError.new(204, "")
+        {
+            status: 204,
+            room_id: params[:room_id],
+        }
+      else
+        action
+      end
+    end
+
   end
 end
